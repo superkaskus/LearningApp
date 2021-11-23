be34ce7ca432be192abc9954ae3e9a1923471b46
Rahim <twarikadams@gmail.com>
2021-11-24 09:41:57 +1100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Adam Abdul-rahim on 24/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
